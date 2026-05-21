6de5a8221f1a17b42966fc7cb59c0d47d1f390ff
felixdiver-debug <felix.diver@icloud.com>
2026-05-21 22:08:18 +1000

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const ENABLE_THINKING_MODE = true; // Set to true to enable chat_template_kwargs
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
-  'llmfan46/gemma-4-E4B-it-ultra-uncensored-heretic-GGUF': 'moonshotai/kimi-k2-thinking',
+  'gpt-4': 'moonshotai/kimi-k2-thinking',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1',
   'claude-3-opus': 'openai/gpt-oss-120b',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
