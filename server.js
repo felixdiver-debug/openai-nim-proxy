1f8cfb4fb7cc000b5c680d81fd18fb27de6f1872
felixdiver-debug <felix.diver@icloud.com>
2026-08-19 17:22:06 +1000

    Update server.js

--- server.js
+++ server.js
@@ -96,8 +96,10 @@ app.post('/v1/chat/completions', async (req, res) => {
       messages: messages,
       temperature: temperature || 0.6,
       max_tokens: max_tokens || 9024,
-      stream: stream || false,
-      ...(ENABLE_THINKING_MODE && { chat_template_kwargs: { thinking: true } })
+      stream: stream || false,...(ENABLE_THINKING_MODE && {
+        ...(nimModel.includes("glm") 
+             ? { thinking: { type: "enabled", reasoning_effort: "max" } } 
+              : { chat_template_kwargs: { thinking: true } })
     };
     
     // Make request to NVIDIA NIM API
